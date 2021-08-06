633cdb5016bfa9d2de0e94fb78d78860cecffc95
Boomaa23 <cmo93003@yahoo.com>
2021-08-05 19:44:45 -0700

    Add building staging and queue

--- backend.php
+++ backend.php
@@ -1,7 +1,11 @@
 <?php
 
+// Backend for sc-bldt-planner
+// Response body consists of print statements
+
 const STORAGE_PATH = "data/storage.json";
 const ITEMS_PATH = "data/items.json";
+const QUEUE_PATH = "data/queue.json";
 
 $reqEndpoint = $_GET["endpoint"];
 $reqData = $_GET["data"];
@@ -10,36 +14,41 @@ if (isset($reqEndpoint) && isset($reqData)) {
     $reqEndpoint = trim(urldecode($reqEndpoint), "/");
     $reqData = urldecode(base64_decode($reqData));
     if (substr_count($reqData, ",") > 0) {
-        $reqData = explode(",", $reqData);
+        $arrData = explode(",", $reqData);
     }
     switch ($reqEndpoint) {
         case "storage/get":
-            getStoredItems();
+            $storage = loadJson(STORAGE_PATH);
+            if ($reqData !== "") {
+                $storage = $storage[$reqData];
+            }
+            print(json_encode($storage));
             break;
         case "storage/add":
-            storeItem($reqData);
+            storeItem($arrData);
             break;
         case "items/search":
             searchItem($reqData);
             break;
+        case "items/exists":
+            $mtlExists = materialExists(loadJson(ITEMS_PATH), $reqData);
+            print($reqData !== "" && $mtlExists ? "true" : "false");
+            break;
+        case "queue/get":
+            $queue = loadJson(QUEUE_PATH);
+            print(json_encode($queue));
+            break;
+        case "queue/add":
+            queueAdd($reqData);
+            break;
+        case "queue/remove":
+            queueRemove($reqData);
+            break;
     }
-}
-
-function getStoredItems() {
-    $storage = loadJson(STORAGE_PATH);
-    print(json_encode($storage));
 }
 
 function storeItem($data) {
-    $items = loadJson(ITEMS_PATH);
-    $mtlExists = false;
-    foreach ($items as $item) {
-        if (isset($item[$data[0]])) {
-            $mtlExists = true;
-            break;
-        }
-    }
-    if ($mtlExists) {
+    if (materialExists(loadJson(ITEMS_PATH), $data[0])) {
         $storage = loadJson(STORAGE_PATH);
         if (!isset($data[1])) {
             $data[1] = 1;
@@ -74,6 +83,17 @@ function storeItem($data) {
     }
 }
 
+function materialExists($validItems, $mtl) {
+    $mtlExists = false;
+    foreach ($validItems as $item) {
+        if (isset($item[$mtl])) {
+            $mtlExists = true;
+            break;
+        }
+    }
+    return $mtlExists;
+}
+
 function searchItem($name) {
     $matchedItems = [];
     $allItems = loadJson(ITEMS_PATH);
@@ -91,11 +111,37 @@ function searchItem($name) {
     }
 }
 
+function queueAdd($data) {
+    $bldgQueue = loadJson(QUEUE_PATH);
+    $jsonData = json_decode($data);
+    $bldg = [];
+    foreach ($jsonData as $mtlName => $mtlQty) {
+        $bldg[$mtlName] = (int) $mtlQty;
+    }
+    $uuid = generateRandomString(4);
+    $bldgQueue[$uuid] = $bldg;
+    saveJson(QUEUE_PATH, $bldgQueue);
+    $respArr = [];
+    $respArr[$uuid] = $bldg;
+    print(json_encode($respArr));
+}
+
+function queueRemove($uuid) {
+    $bldgQueue = loadJson(QUEUE_PATH);
+    unset($bldgQueue[$uuid]);
+    saveJson(QUEUE_PATH, $bldgQueue);
+    print($uuid);
+}
+
 function loadJson($path) {
     return json_decode(file_get_contents($path), true);
 }
 
 function saveJson($path, $data) {
     file_put_contents($path, json_encode($data, JSON_PRETTY_PRINT));
+}
+
+function generateRandomString($length) {
+	return substr(str_shuffle(str_repeat($x='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' , ceil($length/strlen($x)) )),1,$length);
 }
 ?>
--- frontend.js
+++ frontend.js
@@ -10,12 +10,27 @@ document.getElementById("storage-qty-in").onkeypress = function(e) {
     }
 }
 
+document.getElementById("bldg-mtl-in").onkeypress = function(e) {
+    if (e.key === "Enter") {
+        document.getElementById("bldg-mtl-add").click();
+    }
+}
+
+document.getElementById("bldg-qty-in").onkeypress = function(e) {
+    if (e.key === "Enter") {
+        document.getElementById("bldg-mtl-add").click();
+    }
+}
+
 document.getElementById("craft-in").onkeyup = function(e) {
     document.getElementById("search-submit").click();
 }
 
 window.onload = function() {
-    backendRequest("storage/get", "", storeItemCallback);
+    backendRequest("storage/get", "", function(respText) {
+        storeItemCallback(respText);
+        backendRequest("queue/get", "", queueBldgCallback);
+    });
 }
 
 function storeItem() {
@@ -26,6 +41,7 @@ function storeItem() {
 
 function storeItemCallback(respText) {
     var storeOut = document.getElementById("storage-out");
+    const bldgOut = document.getElementById("bldg-out");
 
     const respJson = JSON.parse(respText);
     const respKeys = Object.keys(respJson);
@@ -37,6 +53,14 @@ function storeItemCallback(respText) {
 
     const itemTemplate = document.getElementById("storage-row").content;
     for (var i = 0; i < respKeys.length; i++) {
+        const bldgQueueTrs = bldgOut.querySelectorAll("tr");
+        for (var j = 0; j < bldgQueueTrs.length; j++) {
+            if (bldgQueueTrs[j].innerText.includes(respKeys[i]) && bldgQueueTrs[j].classList.contains("inner-table")) {
+                var qtyCell = bldgQueueTrs[j].cells[2];
+                const ioSep = qtyCell.innerText.indexOf("/");
+                qtyCell.innerText = respJson[respKeys[i]] + qtyCell.innerText.substring(ioSep - 1);
+            }
+        }
         if (respJson[respKeys[i]] === 0) {
             const allStoreTr = storeOut.querySelectorAll("tr");
             for (var j = 0; j < allStoreTr.length; j++) {
@@ -79,6 +103,8 @@ function storeItemCallback(respText) {
     if (storeOut.querySelectorAll("tr").length === 1) {
         storeOut.innerText = "";
     }
+    document.getElementById("storage-in").value = "";
+    document.getElementById("storage-qty-in").value = "1";
 }
 
 function changeItemQty(dir, mtl) {
@@ -128,6 +154,117 @@ function searchItemCallback(respText) {
     }
 }
 
+function addBldgMtl() {
+    const itemName = document.getElementById("bldg-mtl-in").value;
+    backendRequest("items/exists", itemName, addBldgMtlCallback);
+}
+
+function addBldgMtlCallback(respText) {
+    if (respText === "false") {
+        return;
+    }
+
+    var bldgStaging = document.getElementById("bldg-staging");
+    const itemName = document.getElementById("bldg-mtl-in").value;
+    const itemQty = document.getElementById("bldg-qty-in").value;
+
+    if (bldgStaging.innerText === "") {
+        const navHeader = document.getElementById("bldg-staging-navheader").content;
+        bldgStaging.appendChild(navHeader.cloneNode(true))
+    }
+
+    var itemOut = document.getElementById("bldg-staging-row").content.cloneNode(true);
+    itemOut.querySelector("img").src = "data/icons/" + itemName + ".png";
+
+    var itemCols = itemOut.querySelectorAll("td");
+    itemCols[1].innerText = itemName;
+    itemCols[2].innerText = itemQty;
+    
+    bldgStaging.appendChild(itemOut);
+    document.getElementById("bldg-mtl-in").value = "";
+    document.getElementById("bldg-qty-in").value = "1";
+}
+
+function removeBldgMtl(btn) {
+    var rootPn = btn.parentNode.parentNode;
+    
+    if (rootPn.parentNode.getElementsByTagName("tr").length === 2) {
+        rootPn.parentNode.innerText = "";
+    }
+    rootPn.remove();
+}
+
+function queueBldg() {
+    var mtls = {};
+    var mtlRows = document.getElementById("bldg-staging").rows;
+    for (var i = 1; i < mtlRows.length; i++) {
+        var a = mtlRows[i].cells[1].innerText;
+        var b = mtlRows[i].cells[2].innerText;
+        mtls[a] = b;
+    }
+    backendRequest("queue/add", JSON.stringify(mtls), queueBldgCallback);
+}
+
+function queueBldgCallback(respText) {
+    document.getElementById("bldg-staging").innerText = "";
+    var queue = document.getElementById("bldg-out");
+    const mtlRowTemplate = document.getElementById("bldg-queue-mtl").content;
+    const navHeaderTemplate = document.getElementById("bldg-queue-navheader").content;
+
+    const respJson = JSON.parse(respText);
+    const respKeys = Object.keys(respJson);
+
+    for (var i = 0; i < respKeys.length; i++) {
+        var bldg = document.createElement("tr");
+        var rowNavHeader = navHeaderTemplate.cloneNode(true);
+        rowNavHeader.querySelector("td").innerText = respKeys[i];
+        bldg.appendChild(rowNavHeader);
+
+        const mtlJson = respJson[respKeys[i]];
+        const mtlValues = Object.values(mtlJson);
+        const mtlKeys = Object.keys(mtlJson);
+        for (var j = 0; j < mtlKeys.length; j++) {
+            var mtl = mtlRowTemplate.cloneNode(true);
+            mtl.querySelector("img").src = "data/icons/" + mtlKeys[j].toLowerCase().split(' ').join('') + ".png";
+
+            var itemCols = mtl.querySelectorAll("td");
+            itemCols[1].innerText = mtlKeys[j];
+            itemCols[2].innerText = localStorageSearch(mtlKeys[j]) + " / " + mtlValues[j];
+            bldg.appendChild(mtl);
+        }
+        
+        var rmBtnDoc = document.getElementById("bldg-queue-all").content.cloneNode(true);
+        rmBtnDoc.querySelector("button").setAttribute("uuid", respKeys[i]);
+        bldg.appendChild(rmBtnDoc);
+        
+        queue.appendChild(bldg);
+    }
+}
+
+function localStorageSearch(itemName) {
+    const rows = document.getElementById("storage-out").getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        if (rows[i].cells[1].innerText == itemName) {
+            return rows[i].cells[2].innerText;
+        }
+    }
+    return 0;
+}
+
+function removeBldg(btnElem) {
+    backendRequest("queue/remove", btnElem.getAttribute("uuid"), removeBldgCallback);
+}
+
+function removeBldgCallback(respText) {
+    var rows = document.getElementById("bldg-out").getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        if (rows[i].innerHTML.includes(respText)) {
+            rows[i].remove();
+            console.log(rows[i]);
+        }
+    }
+}
+
 function sortItems(tableId, colIdx, desc) {
     var table = document.getElementById(tableId);
     var evalNext = true;
@@ -170,6 +307,7 @@ function sortItems(tableId, colIdx, desc) {
 }
 
 function backendRequest(endpoint, data, callback = null, doLog = true) {
+    console.log("\n");
     var xhr = new XMLHttpRequest();
     var url = "backend.php?endpoint=" + encodeURIComponent(endpoint);
     if (data !== null) {
